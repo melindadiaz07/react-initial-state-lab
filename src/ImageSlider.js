680ee3b64eba0aa157499198ddd0ae0eedfa37ff
Melindadiaz07 <melindadiaz@me.com>
2020-10-26 12:38:21 -0400

    Done

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,27 @@
-// your ImageSlider code here!
+import React, { Component } from 'react'
+
+export default class ImageSlider extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  increment = () => {
+    const newCount = this.state.currentSlideIndex + 1
+    this.setState({
+      currentSlideIndex: newCount
+    })
+  }
+
+
+  render(){
+    return (
+      <div onClick={this.increment}>
+        I am on slide {this.state.currentSlideIndex}
+      </div>
+    )
+  }
+}
